9da55ceeaf0ecd86e299c115d9e4feeb5f513926
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2024-10-18 02:36:44 -0600

    basic template fabric

---
+++ src/main/java/com/example/Example.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Example implements ModInitializer {
+	public static final String MOD_ID = "example";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
